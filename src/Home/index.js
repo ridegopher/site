2fadb609c5623f3423f2dfad5324a9c58578dad6
Chet Lintz <lintzcr@gmail.com>
2018-11-04 17:39:15 -0600

    better site with auth and GQL setup for real

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,19 +1,25 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar/AppBar";
-import Toolbar from "@material-ui/core/Toolbar/Toolbar";
-import Typography from "@material-ui/core/Typography/Typography";
+import React, {Component} from 'react';
+import Grid from "@material-ui/core/Grid/Grid";
 
+class Home extends Component {
+    render() {
+        const isLoggedIn = this.props.authState === 'signedIn';
+        if(!isLoggedIn) {
+            return (
+                <React.Fragment>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            <Grid container justify="center">
+                                <h1>I am the Mother Fucking Marketing Website</h1>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </React.Fragment>
+            )
+        } else {
+            return null
+        }
+    }
+}
 
-const Home = () => (
-    <React.Fragment>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="title" color="inherit">
-                    Ride Gopher
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    </React.Fragment>
-)
-
-export default Home
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import CssBaseline from "@material-ui/core/CssBaseline/CssBaseline";
+import MuiThemeProvider from "@material-ui/core/es/styles/MuiThemeProvider";
+import theme from "./config/theme"
+import Home from "./home";
+import {Authenticator} from 'aws-amplify-react';
+import Amplify from 'aws-amplify';
+import RgAppBar from "./components/RgAppBar";
+import RideGopherApp from "./rgapp";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+Amplify.configure({
+    Auth: {
+        identityPoolId: 'us-east-1:efd0bace-4b74-4521-a5af-5d4f5175a6dc',
+        region: 'us-east-1',
+        mandatorySignIn: true,
+    },
+    aws_appsync_graphqlEndpoint: "https://jalxzy6w4rh77fh3xidwzo7cuu.appsync-api.us-east-1.amazonaws.com/graphql",
+    aws_appsync_region: "us-east-1",
+    aws_appsync_authenticationType: "AWS_IAM",
+});
+
+class Index extends Component {
+    render() {
+        return (
+            <MuiThemeProvider theme={theme}>
+                <div className="App">
+                    <CssBaseline/>
+                    <Authenticator hideDefault>
+                        <RgAppBar/>
+                        <Home/>
+                        <RideGopherApp/>
+                    </Authenticator>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
